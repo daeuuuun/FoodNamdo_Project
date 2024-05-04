76957a8652110e29a6930efb73bad5ac32c974b1
fdoom <11jjklol@naver.com>
2024-05-04 13:19:58 +0900

    페이징 및 정렬 추가

--- backend/imageSearchRecommend/chromadb_utils.py
+++ backend/imageSearchRecommend/chromadb_utils.py
@@ -49,7 +49,7 @@ def img_saving(collection, save_path, table, data, columns):
 keys_to_remove = ["embeddings", "documents", "uris", "data"]
 
 # 이미지로 음식점 이미지 유사도 검색
-def img_to_img(collection, file, file_extension_name, similarity, table):
+def img_to_img(collection, file, file_extension_name, similarity, n_results, table):
     save_path = "./img/temp/"
     os.makedirs(save_path, exist_ok=True)
     unique_filename = f"{uuid.uuid4()}.{file_extension_name}"
@@ -57,7 +57,7 @@ def img_to_img(collection, file, file_extension_name, similarity, table):
     url = save_path + unique_filename
     with open(url, "wb") as f:
         f.write(file.file.read())
-    query_result = collection.query(query_uris=url)
+    query_result = collection.query(query_uris=url, n_results=n_results)
     
     for key in keys_to_remove:
         query_result.pop(key, None)
