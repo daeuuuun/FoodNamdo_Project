8022760453902551c7667050676f5ae745baf936
daeun <eowlfhd1@daum.net>
2024-05-06 13:31:53 +0900

    헤더 링크 연결

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = document.getElementById('root');
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  root
-);
+
+createRoot(document.getElementById('root')).render(<App />);
