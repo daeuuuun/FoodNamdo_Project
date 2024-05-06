7f06b5fdf918012459217800934bc5b07f42e77a
belljin06 <pjj0607@office.kumoh.ac.kr>
2024-05-06 12:21:43 +0900

    cors

--- backend/FoodNamdo/src/main/java/org/zerock/foodnamdo/config/CustomSecurityConfig.java
+++ backend/FoodNamdo/src/main/java/org/zerock/foodnamdo/config/CustomSecurityConfig.java
@@ -22,6 +22,9 @@ import org.zerock.foodnamdo.security.filter.RefreshTokenFilter;
 import org.zerock.foodnamdo.security.filter.TokenCheckFilter;
 import org.zerock.foodnamdo.security.handler.APILoginSuccessHandler;
 import org.zerock.foodnamdo.util.JWTUtil;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Log4j2
@@ -67,10 +70,24 @@ public class CustomSecurityConfig {
         http.addFilterBefore(tokenCheckFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         http.addFilterBefore(new RefreshTokenFilter("/refreshToken", jwtUtil), TokenCheckFilter.class);
-
+        http.cors(cors -> {
+            CorsConfigurationSource source = corsConfigurationSource();
+            cors.configurationSource(source);
+        });
         http.csrf(config -> config.disable());
         http.sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // 여기서 * 대신에 허용할 원본 도메인을 지정할 수 있습니다.
+        configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
+        configuration.addAllowedHeader("*"); // 모든 헤더 허용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     private TokenCheckFilter tokenCheckFilter(JWTUtil jwtUtil) {
